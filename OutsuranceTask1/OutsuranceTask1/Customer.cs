c91bd58ecf76c24182aaf0bcc1ed0045df95f817
JJManeschijn <kmaneschijn@gmail.com>
2017-02-28 10:09:25 +0200

    Add files via upload

---
+++ OutsuranceTask1/OutsuranceTask1/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace OutsuranceTask1
+{
+    public class Customer
+    {
+        public List<CustomerName> CustomerNames { get; set; }
+        public CustomerAddress CustomerAddress { get; set; }
+    }
+
+    public class CustomerName
+    {
+        public string Name { get; set; }
+    }
+
+    public class CustomerAddress
+    {
+        public string Number { get; set; } //we will use string in case of alpha characters in home number e.g. 9a Smith Str, 9b Smith Str etc.
+        public string Name { get; set; }
+    }
+
+}
